bd4616626e1e4e6ed4765599007e125fb37b756d
Derick <derick@talkenterprises.com>
2017-03-09 12:30:32 -0500

    components tree

--- client/components/people/PeopleList.jsx
+++ client/components/people/PeopleList.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Person from './Person.jsx';
-import Follow from './Follow.jsx';
 //const demoPeople = [{type:"text", time:"10:00AM", author:"Someone", content:"Hello World!"}, {type:"image", time:"10:00AM", author:"Someone", content:"http://google.com"}];
 
 
@@ -10,7 +9,6 @@ export default class PeopleList extends React.Component {
             <div>
               {/* Map that */}
               <Person />
-              <Follow />
               {/* endMap */}
             </div>
             )

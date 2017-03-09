d838868e2919dae1747f219de790217463d51d56
Derick <derick@talkenterprises.com>
2017-03-09 10:21:54 -0500

    menu style

--- client/components/root/MenuBar.jsx
+++ client/components/root/MenuBar.jsx
@@ -5,8 +5,9 @@ var FontAwesome = require('react-fontawesome');
 const MenuBar = () => {
   return (
     <div className="MenuBar">
-      <Link to="/profile"><FontAwesome name='user' size='2x' /><span className='tag'>Profile</span></Link>
-      <Link to="/feed"><span className='tag'>Feed</span></Link>
+      <Link to="/profile" activeClassName="active"><FontAwesome name='user' size='2x' /><span className='tag'>Profile</span></Link>
+      <Link to="/feed" activeClassName="active"><FontAwesome name='list-alt' size='2x' /><span className='tag'>Feed</span></Link>
+      <Link to="/people" activeClassName="active"><FontAwesome name='users' size='2x' /><span className='tag'>People</span></Link>
     </div>
   )
 }

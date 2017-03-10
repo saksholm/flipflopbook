b99938b2377e393151171a1708bdf1a2ed7e6166
Joni Saksholm <joni.saksholm@bertschi.fi>
2017-03-10 11:59:53 +0000

    styles+icon for logout

--- client/components/root/MenuBar.jsx
+++ client/components/root/MenuBar.jsx
@@ -15,7 +15,7 @@ export default class MenuBar extends React.Component {
         <Link to="/profile" activeClassName="active"><FontAwesome name='user' /><span className='tag'>Profile</span></Link>
         <Link to="/feed" activeClassName="active"><FontAwesome name='list-alt' /><span className='tag'>Feed</span></Link>
         <Link to="/people" activeClassName="active"><FontAwesome name='users' /><span className='tag'>People</span></Link>
-        <div className="logout" onClick={e => (this.handleLogout(e))}>logout</div>
+        <Link to="#" activeClassName="active"><FontAwesome name='sign-out' /><div className="logout" onClick={e => (this.handleLogout(e))}>logout</div></Link>
       </div>
           )
         }

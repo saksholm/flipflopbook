de86a9db6d38a78b27ee24948b1e5818379473d8
Derick <derick@talkenterprises.com>
2017-03-10 08:48:23 -0500

    follow nb in profile

--- client/components/profile/ProfileInfo.jsx
+++ client/components/profile/ProfileInfo.jsx
@@ -10,15 +10,29 @@ class ProfileInfo extends React.Component {
         if(this.props.data.error) {
           return <div>Sorry, some errors :F, {JSON.stringify(this.props.data.error) }</div>
         }
+        const followersNb = this.props.data.currentUser.followers.length;
+        const followingNb = this.props.data.currentUser.followee.length;
         return (
             <div className='ProfileInfo'>
               <div className="profilePicture">
                 <img src={this.props.data.currentUser.image} alt='Profile' />
               </div>
-              <div className="profileName">
-                {this.props.data.currentUser.username}
+              <div className="infoContainer">
+                <div className="profileName">
+                  {this.props.data.currentUser.username}
+                </div>
+                <div className="optionalInfo">
+                  <div className="locationInfo">
+
+                  </div>
+                  <div className="followInfo">
+                    <div className="followers">Followed by : {followersNb} flipfloppers</div>
+                    <div className="following">Folowing : {followingNb} flipfloppers</div>
+                  </div>
+                </div>
               </div>
-              {/* optional info */}
+
+
               {/* Edit state */}
             </div>
             )
@@ -38,6 +52,22 @@ const query = gql`
         name
         lat
         lng
+      }
+      followers{
+        _id
+        firstName
+        lastName
+        username
+        image
+        facebookID
+      }
+      followee{
+        _id
+        firstName
+        lastName
+        username
+        image
+        facebookID
       }
     }
   }

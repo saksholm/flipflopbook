e9d5c6158c557ca5fccbbaf43fd3ae4ecf56173f
Derick <derick@talkenterprises.com>
2017-03-10 09:19:41 -0500

    feed update

--- client/components/profile/ProfileInfo.jsx
+++ client/components/profile/ProfileInfo.jsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import gql from 'graphql-tag';
-import {graphql} from 'react-apollo';
 
-class ProfileInfo extends React.Component {
+
+export default class ProfileInfo extends React.Component {
     render() {
-        if(this.props.data.loading) {
-          return <div>Loading data... please wait!</div>
-        }
-        if(this.props.data.error) {
-          return <div>Sorry, some errors :F, {JSON.stringify(this.props.data.error) }</div>
-        }
-        const followersNb = this.props.data.currentUser.followers.length;
-        const followingNb = this.props.data.currentUser.followee.length;
+
+        const followersNb = this.props.currentUser.followers.length;
+        const followingNb = this.props.currentUser.followee.length;
         return (
             <div className='ProfileInfo'>
               <div className="profilePicture">
-                <img src={this.props.data.currentUser.image} alt='Profile' />
+                <img src={this.props.currentUser.image} alt='Profile' />
               </div>
               <div className="infoContainer">
                 <div className="profileName">
-                  {this.props.data.currentUser.username}
+                  {this.props.currentUser.username}
                 </div>
                 <div className="optionalInfo">
                   <div className="locationInfo">
@@ -38,47 +32,3 @@ class ProfileInfo extends React.Component {
             )
     }
 }
-
-
-const query = gql`
-  query currentUser {
-    currentUser {
-      firstName
-      lastName
-      username
-      image
-      facebookID
-      location {
-        name
-        lat
-        lng
-      }
-      followers{
-        _id
-        firstName
-        lastName
-        username
-        image
-        facebookID
-      }
-      followee{
-        _id
-        firstName
-        lastName
-        username
-        image
-        facebookID
-      }
-    }
-  }
-`;
-
-const ProfileInfoWithData = graphql(query, {
-  options: ownProps => {
-    return {
-      pollInterval: 500,
-    }
-  },
-})(ProfileInfo);
-
-export default ProfileInfoWithData;
